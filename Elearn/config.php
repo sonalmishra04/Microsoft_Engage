b2eb1c146f84d89ff5942b38807ce2db64bab535
Sonal Mishra <sonalmishra156@gmail.com>
2021-11-26 23:56:15 +0530

    Deleted some files

--- Elearn/config.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$conn = mysqli_connect("localhost", "root", "", "learnp");
-
-
-if(!$conn){
-    echo "Connection failed...";
-}
-?>
--- Elearn/consuc.php
+++
@@ -1,4 +0,0 @@
-<?php
-    mail('sonalmishra156@gmail.com', $_POST['email'], $_POST['message']);
-?>
-<p>Your email has been sent.</p>
